5fb7c2472ebf04ce9680fef0162192fe121f030d
samvarankashyap <samvaran2best@gmail.com>
2015-11-08 16:42:00 -0600

    added naive search algorithm

---
+++ naive_pattern_search.py
@@ -0,0 +1,14 @@
+txt = "AABAACAADAABAAABAA"
+pat = "AABA"
+
+def naive_search(text , pattern):
+     len_of_text = len(text)
+     len_of_pattern = len(pattern)
+     for i in range(0,len_of_text):
+         #print i
+         #print text[i:i+len_of_pattern]
+         #print pattern
+         if  text[i:i+len_of_pattern] == pattern:
+             print "pattern found at index" + str(i)
+    
+naive_search(txt,pat)
