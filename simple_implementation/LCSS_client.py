e286084eec77c3ff4854e6ff89bbf3178244ac49
samvarankashyap <samvaran2best@gmail.com>
2015-11-26 17:12:35 -0600

    removed unecessary files

--- simple_implementation/LCSS_client.py
+++
@@ -1,7 +0,0 @@
-import string_algos
-from string_algos import LCSS
-text ="AGGTAB"
-pattern = "GXTXAYB"
- 
-l = LCSS.LCSS(text,pattern)
-print l.search_sequence()
--- simple_implementation/NaiveSearch_client.py
+++
@@ -1,7 +0,0 @@
-import string_algos
-from string_algos import NaiveSearch
-text ="AGTAdasdasdasdadBdsadasdsadasdasdssadad"
-pattern = "das"
- 
-l = NaiveSearch.NaiveSearch(text,pattern)
-print l.search_pattern()
