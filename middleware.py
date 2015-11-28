bbf755864e6d0ca0c5616f67ceef7de107784e29
samvarankashyap <samvaran2best@gmail.com>
2015-11-28 16:26:21 -0600

    changed middleware pattern check

--- middleware.py
+++ middleware.py
@@ -1,3 +1,5 @@
+import time
+from string_algos import *
 def plagiot(post_obj):
     print "inside plag_check"
     output_obj = "nothing"
@@ -10,6 +12,19 @@ def pattern_check(post_obj):
     p_text = post_obj['patterntext']
     p_algo = post_obj['pattern_algorithm']
     a_text = post_obj['actualtext']
-    if p_algo == 'nss':
-         
+    algo_obj = get_algo(p_algo)
+    start_time = time.time()
+    output_obj = algo_obj.search_pattern()
+    execution_time = time.time() - start_time
+    output_obj['execution_time']=execution_time
     return output_obj
+
+def get_algo(p_algo):
+    if p_algo == 'nss':
+        return NaiveSearch.NaiveSearch(text,pattern)
+    elif p_algo == 'kmp':
+        return KMP.KMP(text,pattern)
+    elif p_algo == 'lcss':
+        return  LCSS.LCSS(text,pattern) 
+    elif p_algo == 'bm':
+        return "not implemented" 
