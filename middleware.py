b7c617572f822af9b4568a90ca11d88369f20617
samvarankashyap <samvaran2best@gmail.com>
2015-11-28 16:12:36 -0600

    added pattern check middleware

--- middleware.py
+++ middleware.py
@@ -2,3 +2,14 @@ def plagiot(post_obj):
     print "inside plag_check"
     output_obj = "nothing"
     return output_obj
+
+def pattern_check(post_obj):
+    output_obj= post_obj
+    print "inside the pattern check"
+    print "processing the pattern match algorithm"
+    p_text = post_obj['patterntext']
+    p_algo = post_obj['pattern_algorithm']
+    a_text = post_obj['actualtext']
+    if p_algo == 'nss':
+         
+    return output_obj
